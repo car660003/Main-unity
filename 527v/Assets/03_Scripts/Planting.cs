f37050d02144a898f3cd6ffa31243981540daf9b
car660003 <39789617+car660003@users.noreply.github.com>
2018-06-03 19:30:53 +0800

    校正南瓜座標

---
+++ 527v/Assets/03_Scripts/Planting.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Growing : MonoBehaviour {
+
+	public Transform player_transform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = Vector3.Distance( player_transform.position, transform.position );
+	}
+}
