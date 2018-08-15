baca494ad512ae8cccf78090431c5d46d95ced74
car660003 <39789617+car660003@users.noreply.github.com>
2018-08-15 21:38:08 +0800

    8.15 將種植當下時間存起來
    功能還沒做完

--- 星球移植2/Assets/02_Scripts/createCrop.cs
+++ 星球移植2/Assets/02_Scripts/createCrop.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class createCrop : MonoBehaviour {
+
+	public string plantingTime_toString;
+	public string plantingTime_toString2;
 
 	// Use this for initialization
 	void Start () {
@@ -21,12 +25,20 @@ public class createCrop : MonoBehaviour {
 
 			Destroy(seed.gameObject); //刪除碰撞到的物件(Seed)
 
-			GameObject pfb = Resources.Load ("pumpkin_01") as GameObject;
+			DateTime plantingTime = DateTime.Now;//把種植當下時間存起來
+			DateTime aaa = plantingTime.AddMinutes(5);
+
+			plantingTime_toString = aaa.ToString ();//把種植當下時間列出來
+			plantingTime_toString2 = aaa.ToString ();
+
+			GameObject pfb = Resources.Load ("pumpkin/pumpkin_01") as GameObject;//產生Pumpkin
 			GameObject prefabInstance = Instantiate (pfb);
 			prefabInstance.transform.parent = this.transform;//設為子物件
-			prefabInstance.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z);
-			prefabInstance.transform.rotation = Quaternion.Euler( -90, 0, 0);
+			prefabInstance.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y+0.1f, this.transform.position.z);
+			//prefabInstance.transform.rotation = Quaternion.Euler( -90, 0, 0);
 			//StartCoroutine (toPlantState02 (10));
 		}
 	}
+
+
 }
