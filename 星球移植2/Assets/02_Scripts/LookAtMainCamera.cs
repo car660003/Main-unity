f64ac158063978014354091786aab4a7093a485f
car660003 <39789617+car660003@users.noreply.github.com>
2018-09-18 00:13:12 +0800

    加上代表水量的plane
    目前生成的植物無法隨機旋轉一個角度，
    plane可看相攝影機，但無法靜止不動

---
+++ 星球移植2/Assets/02_Scripts/LookAtMainCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMainCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}	
+	// Update is called once per frame
+	void Update () {
+
+
+		//当前对象始终面向摄像机。
+		this.transform.LookAt(Camera.main.transform.position);
+		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(Camera.main.transform.position - this.transform.position),0);
+	}
+}
