b6e3152103da46fa2c02d66818ccbf08efacb6bb
car660003 <39789617+car660003@users.noreply.github.com>
2018-09-28 00:59:04 +0800

    灑水器更新
    目前生成的植物無法隨機旋轉一個角度
    田的onParticalCollition的問題暫時解決，但所有田都會觸發(bug)
    灑水器換上最新的，灑水器的粒子也換新的
    植物成熟時的粒子

--- 星球移植2/Assets/02_Scripts/VegetableDetail_Static.cs
+++ 星球移植2/Assets/02_Scripts/VegetableDetail_Static.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class VegetableMainMessange{
-	public static string playerChoice = "";
+	public static int money = 0;
 }
 
 public static class VegetableDetail_Static {
