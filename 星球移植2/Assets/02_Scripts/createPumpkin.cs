3abe717ac706cedc589db985ad1e38e0d4a6cadc
car660003 <39789617+car660003@users.noreply.github.com>
2018-10-04 07:52:02 +0800

    修復植物prefab
    放入所有植物模型，所有植物大致調整好大小，並還沒有程式碼可以生成模型
    缺紅蘿蔔(?)
    沒有死亡的模型
    目前生成的植物無法隨機旋轉一個角度

--- 星球移植2/Assets/02_Scripts/createPumpkin.cs
+++ 星球移植2/Assets/02_Scripts/createPumpkin.cs
@@ -137,7 +137,7 @@ public class createPumpkin : MonoBehaviour {
 		GameObject pfb = Resources.Load ("pumpkin/pumpkin_01") as GameObject;//產生Pumpkin
 		GameObject prefabInstance = Instantiate (pfb);
 		prefabInstance.transform.parent = this.transform;//設為子物件
-		prefabInstance.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y+0.9f, this.transform.position.z);
+		prefabInstance.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y+1f, this.transform.position.z);
 		//float rot = Random.rotation.z;
 		/*Quaternion quate = Quaternion.identity;
 		float Rot_z = Random.rotation.z;
@@ -151,21 +151,21 @@ public class createPumpkin : MonoBehaviour {
 		GameObject pfb = Resources.Load ("pumpkin/pumpkin_02") as GameObject;//產生Pumpkin
 		GameObject prefabInstance = Instantiate (pfb);
 		prefabInstance.transform.parent = this.transform;//設為子物件
-		prefabInstance.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y+0.9f, this.transform.position.z);
+		prefabInstance.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y+1f, this.transform.position.z);
 	}
 	public void PumpkinGrowing_03(){
 		Destroy (transform.GetChild (2).gameObject);
 		GameObject pfb = Resources.Load ("pumpkin/pumpkin_03") as GameObject;//產生Pumpkin
 		GameObject prefabInstance = Instantiate (pfb);
 		prefabInstance.transform.parent = this.transform;//設為子物件
-		prefabInstance.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y+0.9f, this.transform.position.z);
+		prefabInstance.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y+1f, this.transform.position.z);
 	}
 	public void PumpkinGrowing_04(){
 		Destroy (transform.GetChild (2).gameObject);
 		GameObject pfb = Resources.Load ("pumpkin/pumpkin_04") as GameObject;//產生Pumpkin
 		GameObject prefabInstance = Instantiate (pfb);
 		prefabInstance.transform.parent = this.transform;//設為子物件
-		prefabInstance.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y+0.9f, this.transform.position.z);
+		prefabInstance.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y+1f, this.transform.position.z);
 	}
 }
 
