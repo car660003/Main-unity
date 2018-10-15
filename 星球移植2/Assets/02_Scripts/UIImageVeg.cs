d02b9dd81f2f6e67725fa3720b34d223a839bddd
car660003 <39789617+car660003@users.noreply.github.com>
2018-10-15 15:41:42 +0800

    新增UIImageVeg.cs
    種植種子之後要澆水後，changeWaterVolume才會顯示100%水
    增加UIImageVeg更換圖片

---
+++ 星球移植2/Assets/02_Scripts/UIImageVeg.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIImageVeg : MonoBehaviour {
+
+	public int pumpkinCount;
+	public int cucumberCount;
+	public int carrotCount;
+	public int eggplantCount;
+	public int tomatoCount;
+
+	public bool changeFlag = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		if((pumpkin.Count-pumpkinCount)>=1){
+			gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)Resources.Load<Sprite> ("UI/收成圖案-南瓜");
+		}else if((cucumber.Count-cucumberCount)>=1){
+			gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)Resources.Load<Sprite> ("UI/收成圖案-番茄");
+		}else if((carrot.Count-carrotCount)>=1){
+			gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)Resources.Load<Sprite> ("UI/收成圖案-紅蘿蔔");
+		}else if((eggplant.Count-eggplantCount)>=1){
+			gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)Resources.Load<Sprite> ("UI/收成圖案-茄子");
+		}else if((tomato.Count-tomatoCount)>=1){
+			gameObject.GetComponent<SpriteRenderer> ().sprite = (Sprite)Resources.Load<Sprite> ("UI/收成圖案-番茄");
+		}
+
+		pumpkinCount = pumpkin.Count;
+		cucumberCount = cucumber.Count;
+		carrotCount = carrot.Count;
+		eggplantCount = eggplant.Count;
+		tomatoCount = tomato.Count;
+
+	}
+}
--- 星球移植2/Assets/02_Scripts/changeWaterVolume.cs
+++ 星球移植2/Assets/02_Scripts/changeWaterVolume.cs
@@ -28,7 +28,7 @@ public class changeWaterVolume : MonoBehaviour {
 		rend =  GetComponentInChildren<Renderer>();
 		rend.material = meshRender;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		createPumpkin = gameObject.GetComponentInParent<createPumpkin>();
@@ -37,95 +37,140 @@ public class changeWaterVolume : MonoBehaviour {
 		createCucumber = gameObject.GetComponentInParent<createCucumber>();
 		createEggplant = gameObject.GetComponentInParent<createEggplant>();
 
-		if(createPumpkin){
+		if(createPumpkin&&waterLevel.water>=3){
 			hp = createPumpkin.hp;
-			if(hp>=pumpkin.hp){
-				meshRender = Resources.Load("showWaterVolume/Materials/100水量") as Material;
+			if (createPumpkin.remainingSeconds_toString.Equals ("已成熟!")) {
+				meshRender = Resources.Load ("showWaterVolume/Materials/成熟") as Material;
 				rend.material = meshRender;
-			}else if(hp>=(pumpkin.hp*0.75)&&hp<=(pumpkin.hp*0.99)){
-				meshRender = Resources.Load("showWaterVolume/Materials/75水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(pumpkin.hp*0.50)&&hp<=(pumpkin.hp*0.74)){
-				meshRender = Resources.Load("showWaterVolume/Materials/50水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(pumpkin.hp*0.25)&&hp<=(pumpkin.hp*0.49)){
-				meshRender = Resources.Load("showWaterVolume/Materials/25水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(pumpkin.hp*0.10)&&hp<=(pumpkin.hp*0.24)){
-				meshRender = Resources.Load("showWaterVolume/Materials/0水量") as Material;
-				rend.material = meshRender;
+			} else {
+				if(hp>=pumpkin.hp){
+					meshRender = Resources.Load("showWaterVolume/Materials/100水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(pumpkin.hp*0.75)&&hp<=(pumpkin.hp*0.99)){
+					meshRender = Resources.Load("showWaterVolume/Materials/75水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(pumpkin.hp*0.50)&&hp<=(pumpkin.hp*0.74)){
+					meshRender = Resources.Load("showWaterVolume/Materials/50水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(pumpkin.hp*0.25)&&hp<=(pumpkin.hp*0.49)){
+					meshRender = Resources.Load("showWaterVolume/Materials/25水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(pumpkin.hp*0.01)&&hp<=(pumpkin.hp*0.24)){
+					meshRender = Resources.Load("showWaterVolume/Materials/0水量") as Material;
+					rend.material = meshRender;
+				}else if(hp<=(pumpkin.hp*0.01)){
+					meshRender = Resources.Load("showWaterVolume/Materials/按鈕-叉叉") as Material;
+					rend.material = meshRender;
+				}
 			}
-		}else if(createCarrot){
+
+		}else if(createCarrot&&waterLevel.water>=3){
 			hp = createCarrot.hp;
-			if(hp>=carrot.hp){
-				meshRender = Resources.Load("showWaterVolume/Materials/100水量") as Material;
+			if (createCarrot.remainingSeconds_toString.Equals ("已成熟!")) {
+				meshRender = Resources.Load ("showWaterVolume/Materials/成熟") as Material;
 				rend.material = meshRender;
-			}else if(hp>=(carrot.hp*0.75)&&hp<=(carrot.hp*0.99)){
-				meshRender = Resources.Load("showWaterVolume/Materials/75水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(carrot.hp*0.50)&&hp<=(carrot.hp*0.74)){
-				meshRender = Resources.Load("showWaterVolume/Materials/50水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(carrot.hp*0.25)&&hp<=(carrot.hp*0.49)){
-				meshRender = Resources.Load("showWaterVolume/Materials/25水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(carrot.hp*0.10)&&hp<=(carrot.hp*0.24)){
-				meshRender = Resources.Load("showWaterVolume/Materials/0水量") as Material;
-				rend.material = meshRender;
+			} else {
+
+				if(hp>=carrot.hp){
+					meshRender = Resources.Load("showWaterVolume/Materials/100水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(carrot.hp*0.75)&&hp<=(carrot.hp*0.99)){
+					meshRender = Resources.Load("showWaterVolume/Materials/75水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(carrot.hp*0.50)&&hp<=(carrot.hp*0.74)){
+					meshRender = Resources.Load("showWaterVolume/Materials/50水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(carrot.hp*0.25)&&hp<=(carrot.hp*0.49)){
+					meshRender = Resources.Load("showWaterVolume/Materials/25水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(carrot.hp*0.01)&&hp<=(carrot.hp*0.24)){
+					meshRender = Resources.Load("showWaterVolume/Materials/0水量") as Material;
+					rend.material = meshRender;
+				}else if(hp<=(carrot.hp*0.01)){
+					meshRender = Resources.Load("showWaterVolume/Materials/按鈕-叉叉") as Material;
+					rend.material = meshRender;
+				}
 			}
-		}else if(createTomato){
+		}else if(createTomato&&waterLevel.water>=3){
 			hp = createTomato.hp;
-			if(hp>=tomato.hp){
-				meshRender = Resources.Load("showWaterVolume/Materials/100水量") as Material;
+			if (createTomato.remainingSeconds_toString.Equals ("已成熟!")) {
+				meshRender = Resources.Load ("showWaterVolume/Materials/成熟") as Material;
 				rend.material = meshRender;
-			}else if(hp>=(tomato.hp*0.75)&&hp<=(tomato.hp*0.99)){
-				meshRender = Resources.Load("showWaterVolume/Materials/75水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(tomato.hp*0.50)&&hp<=(tomato.hp*0.74)){
-				meshRender = Resources.Load("showWaterVolume/Materials/50水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(tomato.hp*0.25)&&hp<=(tomato.hp*0.49)){
-				meshRender = Resources.Load("showWaterVolume/Materials/25水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(tomato.hp*0.10)&&hp<=(tomato.hp*0.24)){
-				meshRender = Resources.Load("showWaterVolume/Materials/0水量") as Material;
-				rend.material = meshRender;
+			} else {
+
+				if(hp>=tomato.hp){
+					meshRender = Resources.Load("showWaterVolume/Materials/100水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(tomato.hp*0.75)&&hp<=(tomato.hp*0.99)){
+					meshRender = Resources.Load("showWaterVolume/Materials/75水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(tomato.hp*0.50)&&hp<=(tomato.hp*0.74)){
+					meshRender = Resources.Load("showWaterVolume/Materials/50水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(tomato.hp*0.25)&&hp<=(tomato.hp*0.49)){
+					meshRender = Resources.Load("showWaterVolume/Materials/25水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(tomato.hp*0.01)&&hp<=(tomato.hp*0.24)){
+					meshRender = Resources.Load("showWaterVolume/Materials/0水量") as Material;
+					rend.material = meshRender;
+				}else if(hp<=(tomato.hp*0.01)){
+					meshRender = Resources.Load("showWaterVolume/Materials/按鈕-叉叉") as Material;
+					rend.material = meshRender;
+				}
 			}
-		}else if(createCucumber){
+		}else if(createCucumber&&waterLevel.water>=3){
 			hp = createCucumber.hp;
-			if(hp>=cucumber.hp){
-				meshRender = Resources.Load("showWaterVolume/Materials/100水量") as Material;
+			if (createCucumber.remainingSeconds_toString.Equals ("已成熟!")) {
+				meshRender = Resources.Load ("showWaterVolume/Materials/成熟") as Material;
 				rend.material = meshRender;
-			}else if(hp>=(cucumber.hp*0.75)&&hp<=(cucumber.hp*0.99)){
-				meshRender = Resources.Load("showWaterVolume/Materials/75水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(cucumber.hp*0.50)&&hp<=(cucumber.hp*0.74)){
-				meshRender = Resources.Load("showWaterVolume/Materials/50水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(cucumber.hp*0.25)&&hp<=(cucumber.hp*0.49)){
-				meshRender = Resources.Load("showWaterVolume/Materials/25水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(cucumber.hp*0.10)&&hp<=(cucumber.hp*0.24)){
-				meshRender = Resources.Load("showWaterVolume/Materials/0水量") as Material;
-				rend.material = meshRender;
+			} else {
+
+				if(hp>=cucumber.hp){
+					meshRender = Resources.Load("showWaterVolume/Materials/100水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(cucumber.hp*0.75)&&hp<=(cucumber.hp*0.99)){
+					meshRender = Resources.Load("showWaterVolume/Materials/75水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(cucumber.hp*0.50)&&hp<=(cucumber.hp*0.74)){
+					meshRender = Resources.Load("showWaterVolume/Materials/50水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(cucumber.hp*0.25)&&hp<=(cucumber.hp*0.49)){
+					meshRender = Resources.Load("showWaterVolume/Materials/25水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(cucumber.hp*0.01)&&hp<=(cucumber.hp*0.24)){
+					meshRender = Resources.Load("showWaterVolume/Materials/0水量") as Material;
+					rend.material = meshRender;
+				}else if(hp<=(cucumber.hp*0.01)){
+					meshRender = Resources.Load("showWaterVolume/Materials/按鈕-叉叉") as Material;
+					rend.material = meshRender;
+				}
 			}
-		}else if(createEggplant){
+		}else if(createEggplant&&waterLevel.water>=3){
 			hp = createEggplant.hp;
-			if(hp>=eggplant.hp){
-				meshRender = Resources.Load("showWaterVolume/Materials/100水量") as Material;
+			if (createEggplant.remainingSeconds_toString.Equals ("已成熟!")) {
+				meshRender = Resources.Load ("showWaterVolume/Materials/成熟") as Material;
 				rend.material = meshRender;
-			}else if(hp>=(eggplant.hp*0.75)&&hp<=(eggplant.hp*0.99)){
-				meshRender = Resources.Load("showWaterVolume/Materials/75水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(eggplant.hp*0.50)&&hp<=(eggplant.hp*0.74)){
-				meshRender = Resources.Load("showWaterVolume/Materials/50水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(eggplant.hp*0.25)&&hp<=(eggplant.hp*0.49)){
-				meshRender = Resources.Load("showWaterVolume/Materials/25水量") as Material;
-				rend.material = meshRender;
-			}else if(hp>=(eggplant.hp*0.10)&&hp<=(eggplant.hp*0.24)){
-				meshRender = Resources.Load("showWaterVolume/Materials/0水量") as Material;
-				rend.material = meshRender;
+			} else {
+
+				if(hp>=eggplant.hp){
+					meshRender = Resources.Load("showWaterVolume/Materials/100水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(eggplant.hp*0.75)&&hp<=(eggplant.hp*0.99)){
+					meshRender = Resources.Load("showWaterVolume/Materials/75水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(eggplant.hp*0.50)&&hp<=(eggplant.hp*0.74)){
+					meshRender = Resources.Load("showWaterVolume/Materials/50水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(eggplant.hp*0.25)&&hp<=(eggplant.hp*0.49)){
+					meshRender = Resources.Load("showWaterVolume/Materials/25水量") as Material;
+					rend.material = meshRender;
+				}else if(hp>=(eggplant.hp*0.01)&&hp<=(eggplant.hp*0.24)){
+					meshRender = Resources.Load("showWaterVolume/Materials/0水量") as Material;
+					rend.material = meshRender;
+				}else if(hp<=(eggplant.hp*0.01)){
+					meshRender = Resources.Load("showWaterVolume/Materials/按鈕-叉叉") as Material;
+					rend.material = meshRender;
+				}
 			}
 		}
 
@@ -134,7 +179,7 @@ public class changeWaterVolume : MonoBehaviour {
 
 		//distance = Vector3.Distance (Camera.main.transform.position, transform.position);
 		//if (distance <= 10/*&&waterLevel.isPlanting==true/*&&waterLevel.water>=3*/) {
-			gameObject.SetActive (true);
+		gameObject.SetActive (true);
 		//} else {
 		//	gameObject.SetActive (false);
 		//}
