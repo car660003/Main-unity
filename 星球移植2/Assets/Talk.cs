e6e62dad3362831a1647e8654af5984acfb810ac
vv4148 <vv4148@gmail.ocm>
2018-10-18 08:29:33 +0800

    新手教學+任務UI
    電腦版測試可以使用，vive還沒測試，模型還沒套上

---
+++ 星球移植2/Assets/Talk.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Talk : MonoBehaviour {
+    public Flowchart talkFlowchart;
+    public string onCollosionEnter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(UnityEngine.Collision other)
+    {
+        if (other.gameObject.CompareTag("玩家")) {
+            Block targetBlock = talkFlowchart.FindBlock(onCollosionEnter);
+            talkFlowchart.ExecuteBlock(targetBlock);
+        }
+    }
+   
+}
