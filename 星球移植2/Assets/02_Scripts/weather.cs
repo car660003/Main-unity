71f1092e952e029765705c4bcfc9451171acd096
car660003 <39789617+car660003@users.noreply.github.com>
2018-10-22 17:17:15 +0800

    增加天氣控制
    天氣控制能播放下雨粒子，但還不能換天空球

---
+++ 星球移植2/Assets/02_Scripts/weather.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weather : MonoBehaviour {
+	
+	[SerializeField]
+	WeatherSTATUS weatherSTATUS;
+
+	[SerializeField]
+	ParticleSystem rainBasic;
+
+
+
+	enum WeatherSTATUS{
+		sunny,
+		rain,
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		weatherSTATUS = WeatherSTATUS.sunny;
+		rainBasic = gameObject.GetComponentInChildren<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(weatherSTATUS){
+		case WeatherSTATUS.rain:
+			rainBasic.GetComponentInChildren<ParticleSystem>().Play();
+			break;
+
+		case WeatherSTATUS.sunny:
+			rainBasic.GetComponentInChildren<ParticleSystem>().Stop();
+			break;
+		}
+	}
+}
