085747fa24a02ccbdb33019018bd1374e6aa0626
vv4148 <vv4148@gmail.com>
2018-10-29 18:12:51 +0800

    修改UI

---
+++ 星球移植2/Assets/Lock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour {
+
+	public GameObject Lockbt;
+
+	public void AcktiveLockbt(){
+		//		Cube.SetActive(true);
+		if (pumpkin.Count==1) 
+		{
+			Lockbt.SetActive (true);
+		}
+		else 
+		{
+			Lockbt.SetActive (false);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
