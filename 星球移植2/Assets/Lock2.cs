8fcbb668461c01651916f122f2469065d4aeb9b7
vv4148 <vv4148@gmail.ocm>
2018-11-06 09:10:57 +0800

    蒐集冊上鎖功能完成

---
+++ 星球移植2/Assets/Lock2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock2 : MonoBehaviour {
+
+	public GameObject Lockbt;
+
+	public void AcktiveLockbt(){
+		if (cucumber.Count>=1) 
+		{
+			Lockbt.SetActive (true);
+		}
+		else 
+		{
+			Lockbt.SetActive (false);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		AcktiveLockbt();
+
+	}
+}
---
+++ 星球移植2/Assets/Lock4.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock4 : MonoBehaviour {
+
+	public GameObject Lockbt;
+	public void AcktiveLockbt(){
+		if (eggplant.Count>=1) 
+		{
+			Lockbt.SetActive (true);
+		}
+		else 
+		{
+			Lockbt.SetActive (false);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		AcktiveLockbt();
+
+	}
+}
