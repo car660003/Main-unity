daf6a6267d0442c4db8accfc410ade4a86a371c7
car660003 <39789617+car660003@users.noreply.github.com>
2018-11-07 04:00:28 +0800

    商店先亮，2秒後介面才出現
    加上菜單內的種子和成熟作物的顯示text程式碼

---
+++ 星球移植2/Assets/02_Scripts/MenuFollower/PlantCanvasSeedCount.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlantCanvasSeedCount : MonoBehaviour {
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.tag=="pumpkin"){
+			text.text = pumpkin.seedAmount.ToString();
+		}else if(gameObject.tag=="cucumber"){
+			text.text = cucumber.seedAmount.ToString();
+		}else if(gameObject.tag=="carrot"){
+			text.text = carrot.seedAmount.ToString();
+		}else if(gameObject.tag=="eggplant"){
+			text.text = eggplant.seedAmount.ToString();
+		}else if(gameObject.tag=="tomato"){
+			text.text = tomato.seedAmount.ToString();
+		}
+	}
+}
--- 星球移植2/Assets/02_Scripts/StoreMenu/OpenStore.cs
+++ 星球移植2/Assets/02_Scripts/StoreMenu/OpenStore.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class OpenStore : MonoBehaviour {
 
 
-	public GameObject store;
+	public GameObject store;//商店menu
+
+	public float open;
+
+	public GameObject storeObj;
+	public Material meshRender = null;
+	public Renderer rend;
 	// Use this for initialization
 	void Start () {
-		
+		meshRender = Resources.Load("商店/Materials/StoreDark") as Material;
+		rend =  storeObj.GetComponentInChildren<Renderer>();
+		rend.material = meshRender;
 	}
 	
 	// Update is called once per frame
@@ -16,14 +24,23 @@ public class OpenStore : MonoBehaviour {
 		
 	}
 
-	void OnTriggerEnter(Collider player){
+	void OnTriggerStay(Collider player){
 		if (player.gameObject.name == "Camera (eye)") {
-			store.SetActive(true);
+			meshRender = Resources.Load("商店/Materials/StoreLight") as Material;
+			rend.material = meshRender;
+			if(open>=2){
+				store.SetActive(true);
+			}
+			open += Time.deltaTime;
 		}
 	}
 	void OnTriggerExit(Collider player){
 		if (player.gameObject.name == "Camera (eye)") {
+			meshRender = Resources.Load("商店/Materials/StoreDark") as Material;
+			rend.material = meshRender;
 			store.SetActive(false);
+			open = 0f;
 		}
 	}
+
 }
