0a5340104905b5df206ccf181094b1f5be13a376
car660003 <39789617+car660003@users.noreply.github.com>
2018-12-13 02:36:14 +0800

    加上結束動畫

---
+++ 星球移植2/Assets/02_Scripts/EndingMovie.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class EndingMovie : MonoBehaviour {
+
+	public VideoPlayer vp;
+	public GameObject endingPlane;
+	public bool playOneShot = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(pumpkin.Count>0&&cucumber.Count>0&&carrot.Count>0&&eggplant.Count>0&&tomato.Count>0){
+			if(playOneShot==false){
+				endingPlane.SetActive (true);
+				vp.Play();
+				playOneShot = true;
+				StartCoroutine (close());
+			}
+		}
+		/*if(!vp.isPlaying){
+			endingPlane.SetActive (false);
+		}*/
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			playOneShot = false;
+		}
+	}
+	IEnumerator close(){
+		yield return new WaitForSeconds(60);
+		endingPlane.SetActive (false);
+	}
+}
